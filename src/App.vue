3e95a228a39b89f8ed5c5f3f456f7f8e53cd29af
jiaqi <1140103268@qq.com>
2019-12-13 17:30:22 +0800

    nav left

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,40 @@
 <template>
   <div id="app">
     <main-header></main-header>
-    <div class="main-cnt">
-      <router-view></router-view>
+    <div class="app-main">
+      <div class="app-main-left">
+        <Menu></Menu>
+      </div>
+      <div class="app-main-right">
+        <router-view/>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-
-
-
-  export default {
-    name: 'app',
-
-  };
+  import Menu from './components/menu.vue';
+export default {
+  name: 'App',
+  components:{
+    Menu
+  }
+}
 </script>
+
+<style lang="stylus">
+#app
+  .app-main
+    display: flex;
+    min-width: 1101px;
+    margin: 0 auto 50px auto;
+    width: 85%;
+    .app-main-left
+      min-width 230px
+      position relative
+    .app-main-right
+      position relative
+      padding-top 15px
+      flex 1
+</style>
+
